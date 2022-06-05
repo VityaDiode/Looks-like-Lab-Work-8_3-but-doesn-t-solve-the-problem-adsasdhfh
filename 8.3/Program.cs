afe86e0f8d378a5c307574f3226e367a68bf7111
Diode <Diode@DESKTOP-NF7280U>
2022-06-05 16:08:05 +0300

    Add project files.

---
+++ 8.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nr_8._3
+{
+    /// <summary>
+    /// хахахахаххахахахаххаха
+    /// хавхфхафвхвхфыахывхвыаф
+    /// 0  тасков из нужных выполнено
+    /// какая музыка какие вены что мне втюхали?
+    /// Где итераторы??7?7?7?77
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            str S = new str();
+
+            string s1 = "Hello World";
+            S.LetterParse(s1);
+
+            string s2 = "one, two, three, four, five";
+            S.WordParse(s2);
+
+            string s3 = "Я себе внутривенно вкалываю музыку. Тихо, без боли, поражает каждую мускулу. Это лучше, даже самого чистого воздуха. До слез пробивает и становится глухо.";
+            S.SentenceParse(s3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 8.3/str.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nr_8._3
+{
+    public class str
+    {
+
+        public void LetterParse(string s1)
+        {
+            foreach (char element in s1)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public void WordParse(string s2)
+        {
+            char[] delimiterChars = { ' ', '?', ',', '.', '/', ':', ';', '!', '+', '-' };
+            string[] words = s2.Split(delimiterChars);
+            foreach (string element in words)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+
+        public void SentenceParse(string s3)
+        {
+            char delimiterChars = '.';
+            string[] words = s3.Split(delimiterChars);
+            foreach (string element in words)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
